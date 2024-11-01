b84e536fa1d7ad7e6b02d0f4cf133cb99cbf8129
as-Solo <asdelsolo@gmail.com>
2024-11-01 14:17:39 +0100

    query basada message

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const verifyToken = require("../middlewares/auth.middlewares");
 const router = require("express").Router();
 const Message = require("../models/Message.model")
+const User = require("../models/User.model")
 const { ObjectId } = require('mongoose').Types;
 
 
@@ -92,41 +93,36 @@ router.get("/group-by/conversation/:userId", verifyToken, async (req, res, next)
     }
 })
 
+// GET "/api/messages/chatlist" => Devuelve toda la lista de conversaciones abiertas
+router.get("/chatlist", verifyToken, async (req, res, next)=>{
+  const loggedUser = req.payload._id
+  try {
+    const listaUsuarios = await User.findById(loggedUser, 'accepted')
+    .populate({
+      path: 'accepted',
+      match: {accepted: loggedUser},
+      select: 'image username createdAt'
+    })
+    .lean(); // Convierte el resultado en un objeto plano para mejor manipulación
+
+    // Añadimos el conteo de seguidores para cada usuario en "accepted"
+    const acceptedUsersWithFollowersCount = await Promise.all(
+      listaUsuarios.accepted.map(async (acceptedUser) => {
+        const followersCount = await User.countDocuments({ follow: acceptedUser._id });
+        const followCount = acceptedUser.follow? acceptedUser.follow.length : 0; ;
+        return { ...acceptedUser, followCount, followersCount }; // Añade el campo "followersCount"
+      })
+    );
+
+    // Devuelve solo los campos requeridos
+    res.status(200).json({
+      accepted: acceptedUsersWithFollowersCount, // Solo devolvemos "accepted" con el conteo de seguidores
+    });
+  }
+  catch (error) {
+    console.log(error)
+    next(error)
+  }
+})
+
 module.exports = router
-
-
-// const messages = await Message.aggregate([
-//     {
-//       $match: {
-//         $or: [
-//           { remitente: userId1, destinatario: userId2 },
-//           { remitente: userId2, destinatario: userId1 }
-//         ]
-//       }
-//     },
-//     {
-//       $addFields: {
-//         day: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } }
-//       }
-//     },
-//     {
-//       $sort: { createdAt: 1 }  // Asegura que los mensajes se ordenen por createdAt ascendente
-//     },
-//     {
-//       $group: {
-//         _id: "$day",
-//         messages: { $push: "$$ROOT" }
-//       }
-//     },
-//     {
-//       $project: {
-//         _id: 0,
-//         day: "$_id",
-//         messages: 1
-//       }
-//     },
-//     {
-//       $sort: { day: 1 }
-//     }
-//   ]);
-  
